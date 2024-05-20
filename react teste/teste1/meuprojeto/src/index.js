4a4ae603aa7279293ad3cb3d01e03adff34059b6
Willian Amorim <110067851+WillianApAmorim@users.noreply.github.com>
2024-05-20 17:41:07 -0300

    revisando react

---
+++ react teste/teste1/meuprojeto/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+    const containerStyles = () => {
+        return{widh:1280, margin: 'auto', border: '1px solid black', backgroundColor: "#EEE", borderRadius:8, padding:12, textAlign:'center'}
+    }
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+<mao />
+
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
